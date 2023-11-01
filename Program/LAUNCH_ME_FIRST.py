5e802b00b92137b06fd5ca8fb233a9bac3e0973e
KakunoFountain <m07273317.0410@gmail.com>
2023-11-02 06:27:30 +0900

    a

--- Program/LAUNCH_ME_FIRST.py
+++ Program/LAUNCH_ME_FIRST.py
@@ -1,17 +1,19 @@
 import subprocess as sp
+from main import main
 
 wmc = r"C:\Users\m0727_d73jsnl\Downloads\PCsoft\WebcamMotionCapture_Win_ja\WebcamMotionCapture.exe"
 VScode = r"C:\Users\m0727_d73jsnl\AppData\Local\Programs\Microsoft VS Code\Code.exe"
-obs = r"C:\Program Files\obs-studio\bin\64bit"
+#obs = r"C:\Program Files\obs-studio\bin\64bit"
 
 wmc_s = sp.Popen(wmc)
 VScode_s = sp.Popen([VScode, "main.py"])
+main()
 #obs_s = sp.run(["start", "obs64.exe"], shell=True)
 #obs_s = sp.run("cd", shell=True, cwd=obs)
 #sp.run(["start", "\OBS Studio (64bit).exe"], shell=True)
 
 
-input("何かを入力し、Enterを押すとWMC,VScodeを終了します。：")
-wmc_s.kill()
-VScode_s.kill()
+#input("何かを入力し、Enterを押すとWMC,VScodeを終了します。：")
+#wmc_s.kill()
+#VScode_s.kill()
 #obs_s.kill()
--- Program/main.py
+++ Program/main.py
@@ -251,5 +251,7 @@ def main():
     """
 
 
+"""
 if __name__ == "__main__":
     main()
+"""
